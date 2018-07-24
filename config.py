6845b7bfca4b6bf2c8b38474e80ec59fc5b78c03
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-24 17:02:32 +0100

    Add configurations for development

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ class MainConfiguration(object):
     """ Main configuration class"""
     DEBUG = False
     CSRF_ENABLED = True
+
+
+class DevelopmentEnvironment(MainConfiguration):
+    """ Configurations for development"""
+    DEBUG = True
