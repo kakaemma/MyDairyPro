7980e75a66157233b8f4a991467bb4b162e8b0e0
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-24 19:13:31 +0100

    Add base class for testing

---
+++ tests/test_base.py
@@ -0,0 +1,8 @@
+import unittest
+from instance.config import application_config
+from app.views import app
+
+class BaseClass(unittest.TestCase):
+    def setUp(self):
+        app.config.from_object(application_config['TestingEnv'])
+        self.client = app.test_client()
