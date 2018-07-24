e05090ec2a7238dfba153d45d267ec4451969e66
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-24 20:16:38 +0100

    Add tests for production environment

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,3 +12,13 @@ class TestDevelopmentEnvironment(TestCase):
     def test_app_in_development_env(self):
         """ Should return debug is true if debug is enabled"""
         self.assertTrue(app.config['DEBUG'])
+
+class TestProductionEnv(TestCase):
+    def create_app(self):
+        app.config.from_object('instance.config.ProductionEnvironment')
+        return app
+
+    def test_app_in_production(self):
+        """ Should return false when debug and testing are off"""
+        self.assertFalse(app.config['DEBUG'])
+        self.assertFalse(app.config['TESTING'])
