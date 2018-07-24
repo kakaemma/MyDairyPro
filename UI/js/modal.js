981dc465e418fd7e24ea0c00018717cb47cc5511
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-24 13:57:26 +0100

    Update modal for modifying entry

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -7,3 +7,13 @@ btn.onclick = function() {
 span.onclick = function() {
     modal.style.display = "none";
 }
+
+var delete_modal = document.getElementById('delModal');
+var btn = document.getElementById("editBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
