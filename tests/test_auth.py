325871edc19488879443583a2b9f8dfae2ea4066
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-24 19:33:33 +0100

    Modify tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,3 +7,10 @@ class TestAuthentication(BaseClass):
         """ Test response for title in the index page """
         response = self.client.get('/')
         self.assertIn('Welcome to My Diary', response.data.decode())
+
+    def test_registration_with_no_values(self):
+        """ Test registration with missing values"""
+        response = self.client.post('/api/v1/register',
+                                    content_type='application/json',
+                                    data=self.empty_reg)
+        self.assertIn('Missing values', response.data.decode())
