fee2bd19b376afa13c4eec8f0c002d6a80cbd84c
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-25 11:10:40 +0100

    Modify init

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_api import FlaskAPI
 from config import application_config
 
 
-
 def create_app(config_name):
     app = FlaskAPI(__name__)
     APP_ROOT = os.path.dirname(app.instance_path)
