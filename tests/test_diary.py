449027aaaca8ab6e2b177c5ea9c6b91f483d42c7
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-25 09:56:57 +0100

    Add tests for addind diary entry successfully

---
+++ tests/test_diary.py
@@ -0,0 +1,21 @@
+from tests.test_base import BaseClass
+
+
+class TestDiary(BaseClass):
+
+    def test_add_diary_without_name(self):
+        response = self.client.post('/api/v1/entries',
+                                    content_type='application/json',
+                                    data=self.empty_diary,
+                                    )
+        self.assertIn('Missing or bad parameter submitted',
+                         response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_diary_successfully(self):
+        response = self.client.post('/api/v1/diary',
+                                    content_type='application/json',
+                                    data=self.new_diary,
+                                   )
+        self.assertIn('Diary successfully added', response.data.decode())
+        self.assertEqual(response.status_code, 201)
