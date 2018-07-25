7d9a30c7fcb193b11d500e8e03457e3f02bf5822
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-25 10:34:17 +0100

    Modify application

--- app/views.py
+++ app/views.py
@@ -1,6 +1,4 @@
-from flask import jsonify, request, render_template
-from app.models import UserModel
-from validate_email import validate_email
+from flask import jsonify, request
 from app import create_app
 
 
@@ -15,34 +13,6 @@ def index():
     """
 
     return "<h2>Welcome to My Diary</h2>"
-
-
-@app.route('/api/<version>/register', methods=['POST'])
-def register(version):
-    """ This end point registers a user
-    :param version: 
-    :return: 
-    """
-    request.get_json(force=True)
-    if 'name' in request.json and 'email' in request.json \
-            and 'password' in request.json:
-        if request.json['name'] and request.json['email'] \
-                and request.json['password']:
-
-            if len(request.json['password']) < 6:
-                return message_to_return(422, 'Password too short')
-
-            if validate_email(request.json['email']):
-                user = UserModel(request.json['name'],
-                                 request.json['email'], request.json['email'])
-
-                register_user = user.add_user()
-                print(register_user)
-                if register_user == None:
-                    return  message_to_return(201)
-                return message_to_return(409, 'email')
-            return message_to_return(422)
-    return message_to_return(400)
 
 
 @app.route('/api/<version>/entries', methods=['POST'])
