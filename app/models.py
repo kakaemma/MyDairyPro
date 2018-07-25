6fffba741bcb7e24b3ee340e9b087f94f09c34d1
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-25 09:31:19 +0100

    Add diary model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class UserModel(object):
     """ This class handles all model operations to the User"""
     users = []
@@ -20,6 +23,28 @@ class UserModel(object):
             if user.email == self.email:
                 return 'exists'
         UserModel.users.append(self)
+
+class DiaryModel(object):
+    diary = []
+
+    def __init__(self, name, desc):
+        """
+        This constructor initialises diary
+        :param name: 
+        """
+        self.diary_id = len(DiaryModel.diary)+1
+        self.name = name
+        self.desc = desc
+        self.date_created = datetime.datetime.utcnow()
+        self.date_modified = None
+
+    def create_diary(self):
+        """
+        Adds diary object to list
+        :return: 
+        """
+        DiaryModel.diary.append(self)
+        return self.diary_id
 
 
 
