6753950d479a2cf9c2e81d5ae9cf2ce2097f7593
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-25 11:13:39 +0100

    Update models

---
+++ app/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+class DiaryModel(object):
+    diary = []
+
+    def __init__(self, name, desc):
+        """
+        This constructor initialises diary
+        :param name: 
+        """
+        self.diary_id = len(DiaryModel.diary)+1
+        self.name = name
+        self.desc = desc
+        self.date_created = datetime.datetime.utcnow()
+        self.date_modified = None
+
+    def create_diary(self):
+        """
+        Adds diary object to list
+        :return: 
+        """
+
+        for this_diary in DiaryModel.diary:
+            if this_diary.name == self.name:
+                return self.name
+        DiaryModel.diary.append(self)
+        
