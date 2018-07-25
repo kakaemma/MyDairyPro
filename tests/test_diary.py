455f2d7649c11f06a6d44b8092b8d373cecb9c52
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-25 10:06:38 +0100

    Add test for existing diary

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -19,3 +19,15 @@ class TestDiary(BaseClass):
                                    )
         self.assertIn('Diary successfully added', response.data.decode())
         self.assertEqual(response.status_code, 201)
+
+    def test_add_diary_with_existing_name(self):
+        self.client.post('/api/v1/entries',
+                         content_type='application/json',
+                         data=self.new_diary,
+                         )
+        response = self.client.post('/api/v1/entries',
+                                    content_type='application/json',
+                                    data=self.new_diary,
+                                    )
+        self.assertIn('Diary already exists', response.data.decode())
+        self.assertEqual(response.status_code, 409)
