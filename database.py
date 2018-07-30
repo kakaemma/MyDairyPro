d23d76ef56eafc8f41653ef06882d9dcbaab34e9
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-30 21:55:35 +0100

    Update database connections

--- database.py
+++ database.py
@@ -8,20 +8,11 @@ class DatabaseConnection():
             self.connection = connect("dbname=mydiary_pro user =admin password=admin host=localhost port=5433")
             self.connection.autocommit = True
             self.cursor = self.connection.cursor()
-            pprint("success")
         except:
             pprint("Can not connect to the database")
 
-    def create_table_users(self):
-        """ This method creates users table in the database"""
-        create_table_query_for_users = ("""CREATE TABLE IF NOT EXISTS 
-                    users(user_id SERIAL PRIMARY KEY,  
-                    name VARCHAR(15) NOT NULL , email VARCHAR (20) NOT NULL,
-                     password VARCHAR(20) NOT NULL); """
-                                        )
-        self.cursor.execute(create_table_query_for_users)
 
-    def create_table_entries(self):
+    def create_tables(self):
         create_table_query_for_entries = (""" CREATE TABLE IF NOT EXISTS 
                                       entries(diary_id SERIAL PRIMARY KEY,
                                       name VARCHAR (15) NOT NULL, 
@@ -32,11 +23,21 @@ class DatabaseConnection():
                                        FOREIGN KEY (user_id)REFERENCES 
                                        users (user_id));""")
 
+        create_table_query_for_users = ("""CREATE TABLE IF NOT EXISTS 
+                    users(user_id SERIAL PRIMARY KEY,  
+                    name VARCHAR(15) NOT NULL , email VARCHAR (20) NOT NULL,
+                     password VARCHAR(20) NOT NULL); """
+                                        )
+
         self.cursor.execute(create_table_query_for_entries)
+        self.cursor.execute(create_table_query_for_users)
+
+    def trancate_table(self, table_name):
+        self.cursor.execute("TRUNCATE TABLE{} RESTART IDENTITY CASCADE"
+                            .format(table_name))
 
 
 
 if __name__ == '__main__':
     database_connection = DatabaseConnection()
-    database_connection.create_table_users()
-    database_connection.create_table_entries()
+    database_connection.create_tables()
