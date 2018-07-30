e92015b7b10fa16168db14151cdad64b08fadf86
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-30 20:02:43 +0100

    Add test fo successfull registration

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -18,3 +18,31 @@ class TestAuth(BaseClass):
                                     data=self.invalid_email)
         self.assertIn('Invalid Email address', response.data.decode())
         self.assertEqual(response.status_code, 422)
+
+    def test_registration_with_short_password(self):
+        """ Should return password too short"""
+        response = self.client.post('/api/v1/auth/signup',
+                                    content_type='application/json',
+                                    data=self.short_password)
+        self.assertIn('Unprocessable entity', response.data.decode())
+        self.assertEqual(response.status_code, 422)
+
+    def test_successful_registration(self):
+        """ Should return registration successful and status code 201"""
+        response = self.client.post('/api/v1/auth/signup',
+                                    content_type='application/json',
+                                    data=self.new_user)
+        self.assertIn('successfully added',
+                      response.data.decode())
+        self.assertEqual(response.status_code, 201)
+
+    def test_registration_with_existing_email(self):
+        """ Should return conflict email already exists"""
+        self.client.post('/api/v1/auth/signup',
+                         content_type='application/json',
+                         data=self.user)
+        response = self.client.post('/api/v1/auth/signup',
+                                    content_type='application/json',
+                                    data=self.user)
+        self.assertIn('already exists', response.data.decode())
+        self.assertEqual(response.status_code, 409)
