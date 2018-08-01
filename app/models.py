156b56204d12abaa231f9d9e4db748c55018de67
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-08-01 08:48:18 +0100

    Add methods to add entry and get all entries

--- app/models.py
+++ app/models.py
@@ -40,19 +40,17 @@ class UserModel(object):
 
 
 class DiaryModel(object):
-    diary = []
 
-    def __init__(self, name, desc):
+    def __init__(self, name, desc, user_id):
         """
         This constructor initialises diary
         :param name: 
         """
-        self.diary_id = len(DiaryModel.diary)+1
+        self.user_id = user_id
         self.name = name
         self.desc = desc
         self.date_created = datetime.datetime.utcnow()
-        self.date_modified = None
-        self.date_modified = None
+        self.date_modified = datetime.datetime.utcnow()
 
     def create_diary(self):
         """
@@ -60,28 +58,38 @@ class DiaryModel(object):
         :return: 
         """
 
-        for this_diary in DiaryModel.diary:
-            if this_diary.name == self.name:
+        try:
+            query_to_search_diary = "SELECT * FROM entries WHERE name=%s"
+            connection.cursor.execute(query_to_search_diary, [self.name])
+            row = connection.cursor.fetchone()
+            if row:
                 return True
-        DiaryModel.diary.append(self)
+            query_to_add_entry = "INSERT INTO entries(name, description, user_id,date_created,date_modified) VALUES(%s,%s,%s,%s,%s)"
+            connection.cursor.execute(query_to_add_entry,(self.name, self.desc, self.user_id, self.date_created, self.date_modified))
+        except Exception as exc:
+            print(exc)
 
     @classmethod
-    def get_entries(cls):
+    def get_entries(cls, user_id):
         """
         This method returns all entries
         :return: 
         """
-        if len(DiaryModel.diary) >= 1:
-            response = []
-            for entry in DiaryModel.diary:
-                response.append({
-                    'id': entry.diary_id,
-                    'name': entry.name,
-                    'Description': entry.desc,
-                    'Date created': entry.date_created,
-                    'Date Modified': entry.date_modified
-                })
-            return response
+        response =[]
+        query_to_get_all_entres = 'SELECT * FROM entries WHERE user_id=%s'
+        connection.cursor.execute(query_to_get_all_entres,[user_id])
+        rows = connection.cursor.fetchall()
+        for row in rows:
+            response.append({
+                'id': row[0],
+                'name': row[1],
+                'Description': row[2],
+                'Date created': row[3],
+                'Date Modified': row[4]
+            })
+        return response
+
+
 
     @staticmethod
     def get_entry(search_id):
