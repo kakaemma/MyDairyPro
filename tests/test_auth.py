7285c8edf770b1fa4280dea40a6375b810c1ef06
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-08-01 17:22:18 +0100

    Update staus code in tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,7 +9,7 @@ class TestAuth(BaseClass):
                                     content_type='application/json',
                                     data=self.empty_reg)
         self.assertIn('Missing values', response.data.decode())
-        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.status_code, 400)
 
     def test_registration_with_invalid_email(self):
         """ Test should return invalid email address"""
@@ -17,7 +17,7 @@ class TestAuth(BaseClass):
                                     content_type='application/json',
                                     data=self.invalid_email)
         self.assertIn('Invalid Email address', response.data.decode())
-        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.status_code, 400)
 
     def test_registration_with_short_password(self):
         """ Should return password too short"""
@@ -25,7 +25,7 @@ class TestAuth(BaseClass):
                                     content_type='application/json',
                                     data=self.short_password)
         self.assertIn('Unprocessable entity', response.data.decode())
-        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.status_code, 400)
 
     def test_successful_registration(self):
         """ Should return registration successful and status code 201"""
