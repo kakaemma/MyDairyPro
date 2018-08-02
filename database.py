4471e72fcef8881adf65675912a1f1ae22ddb14e
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-08-02 18:03:19 +0100

    Modify database connection

--- database.py
+++ database.py
@@ -5,15 +5,38 @@ from psycopg2 import connect
 class DatabaseConnection():
 
     def __init__(self):
-        # self.connection = connect("dbname=mydiary_pro user =admin password=admin host=localhost port=5433")
+        """
+        This constructor creates a connection to the database
+        :param dbname: 
+        :param user: 
+        :param password: 
+        :param host: 
+        :param port: 
+        """
+        # if app_environment == 'TESTING':
+        #     self.connection = connect("dbname=test_db user =admin password=admin host=localhost port=5433")
+        #     self.connection.autocommit = True
+        #     self.cursor = self.connection.cursor()
+        #     #-------------------------------------------
+        #     #When on travis
 
-        self.connection = connect("dbname=mydiary_pro user =postgres password='' host=localhost port=5432")
+        # self.connection = connect("dbname=mydiary_pro user =postgres password='' host=localhost port=5432")
+        # self.connection.autocommit = True
+        # self.cursor = self.connection.cursor()
+
+            #----------------------------------------------
+
+        self.connection = connect("dbname=mydiary_pro user =admin password=admin host=localhost port=5433")
         self.connection.autocommit = True
         self.cursor = self.connection.cursor()
 
 
 
+
+
+
     def create_tables(self):
+        """ This method creates all tables"""
         create_table_query_for_entries = (""" CREATE TABLE IF NOT EXISTS 
                                       entries(diary_id SERIAL PRIMARY KEY,
                                       name VARCHAR (50) NOT NULL, 
@@ -30,11 +53,16 @@ class DatabaseConnection():
                      password VARCHAR(20) NOT NULL); """
                                         )
 
+        # Execute creating tables
         self.cursor.execute(create_table_query_for_users)
         self.cursor.execute(create_table_query_for_entries)
 
 
     def drop_table(self, table_name):
+        """
+        This method truncates a table
+        :param table_name: 
+        :return: 
+        """
         self.cursor.execute("TRUNCATE TABLE {} RESTART IDENTITY CASCADE"
                             .format(table_name))
-
