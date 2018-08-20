aea72475df23828889d6dd3864babd3b321918ef
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-08-21 00:51:27 +0100

    Modify signup script

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -1,20 +1,21 @@
-document.getElementById("register-Form").addEventListener("submit",register)
+document.getElementById("register-Form").addEventListener("submit",register);
 function register(event){
-    event.preventDefault()
+    event.preventDefault();
 
     first_name = document.getElementById('fname').value;
     last_name = document.getElementById('lname').value;
     user_email = document.getElementById('email').value
     user_password = document.getElementById('password');
 
-    uri = 'https://diary-2018.herokuapp.com/api/v1/auth/signup'
-    headers = new Headers(
+    uri = 'https://diary-2018.herokuapp.com/api/v1/auth/signup';
+    request_headers = new Headers(
         {
             'Content-Type': 'application/json'
         }
-    )
+    );
     fetch(uri,{
         method: 'POST',
+        headers:request_headers,
         body:JSON.stringify({
             name: first_name,
             email: user_email,
@@ -23,15 +24,17 @@ function register(event){
         })
 
 
-    }).then((response)=>{
-        if(respose.status_code == 201){
-            window.location='./index.html'
-            alert('Account successfully created created')
-
-    }
-    else {
-            alert()
-    }
+    }).then((response) => {
+        status_code = response.status_code
+        return response.json()   
+    }).then((data) => {
+        if(status_code !=201){
+            document.getElementById("error-msg").innerHTML = data['Error']
+        }
+        else{
+            window.location = './index.html'
+            alert('Account created please login')
+        }
     })
 
 }
