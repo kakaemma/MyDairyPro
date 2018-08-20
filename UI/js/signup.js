a91736455dfa8e2fb627b2bcc7e0c7663452c40b
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-08-20 15:57:31 +0100

    Modify signup script

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -4,8 +4,8 @@ function register(event){
 
     first_name = document.getElementById('fname').value;
     last_name = document.getElementById('lname').value;
-    email = document.getElementById('email').value
-    password = document.getElementById('password');
+    user_email = document.getElementById('email').value
+    user_password = document.getElementById('password');
 
     uri = 'https://diary-2018.herokuapp.com/api/v1/auth/signup'
     headers = new Headers(
@@ -15,8 +15,23 @@ function register(event){
     )
     fetch(uri,{
         method: 'POST',
+        body:JSON.stringify({
+            name: first_name,
+            email: user_email,
+            password: user_password
+
+        })
 
 
+    }).then((response)=>{
+        if(respose.status_code == 201){
+            window.location='./index.html'
+            alert('Account successfully created created')
+
+    }
+    else {
+            alert()
+    }
     })
 
 }
