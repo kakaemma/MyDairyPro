6f9b3a116026f281de07cff2849af6bdb5d2ff41
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-08-28 23:10:06 +0100

    Add messages to return when adding entries

--- UI/js/add_entries.js
+++ UI/js/add_entries.js
@@ -5,9 +5,9 @@
 document.getElementById("add-entry-form").addEventListener('submit', add_entry);
 
 function add_entry(event) {
-    event.preventDefault()
-    diary_name = document.getElementById('add_entry_name').value;
-    description = document.getElementById('add_entry_desc').value;
+    event.preventDefault();
+    let diary_name = document.getElementById('add_entry_name').value;
+    let description = document.getElementById('add_entry_desc').value;
 
     fetch('https://diary-2018.herokuapp.com/api/v1/entries',{
         method:'POST',
@@ -21,7 +21,16 @@ function add_entry(event) {
         })
 
     }).then((response)=>{
-        
+        status_code =response.status;
+        return response.json();
+    }).then((response)=>{
+        if(status_code != 201){
+            alert(response['Error'])
+        }
+        else {
+            window.location= "dashboard.html"
+            alert("Diary successfully added")
+        }
     })
 
 }
