430c8e87f0ccc5e0c99ea93aa856a80b07260040
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-09-02 15:19:55 +0100

    Add method for editing an entry

--- UI/js/entries.js
+++ UI/js/entries.js
@@ -26,27 +26,8 @@
             display_results += '<tr><td><h5>'+entry.name
                 +'</h5></td><td><h5>'+ entry.Description
                 +'</h5></td><td>'+
-                '<a href="#" class="button button_dash" id="modalbtn" onclick="return pop_edit_modal()">Edit</a>'+
-                '</td></tr>'+
-                '        <div class="modal" id="editModal" style="left: 0px">'+
-                '<div class="modal_content">'+
-                    '<div class="modal_header">'+
-                        '<span class="close"><a href="">&times;</a></span>'+
-                        '<h2>Update Diary</h2>'+
-                    '</div>'+
-                    '<div class="modal_body" style="padding-top: 20px;">'+
-                        '<form action="" name="editentry" id="modify-entry" method="PUT">'+
-                            '<div class="form-group" style="margin-bottom: 20px">'+
-                                '<input type="text" name="name" class="formField" placeholder="New Diary name " />'+
-                            '</div>'+
-                            '<div class="form-group">'+
-                                '<input type="text" name="desc" class="formField" placeholder="Diary description " />'+
-                            '</div>'+
-                            '<button class="button button_dash" name="entry_btn" type="submit" onclick="return modify_entry('+entry.id+')">Update</button>'+
-                        '</form>'+
-                    '</div>'+
-                '</div>'+
-            '</div>';
+                '<a href="#" class="button button_dash" id="modalbtn" onclick="return edit_modal('+entry.id+')">Edit</a>'+
+                '</td></tr>';
 
         });
         document.getElementById('diary-display').innerHTML =display_results;
@@ -57,12 +38,50 @@
 
 
 
-function pop_edit_modal () {
+function edit_modal (entry_id) {
 
     document.getElementById('editModal').style.display = "block";
+
+    alert(entry_id)
+document.getElementById('modify-entry').addEventListener('submit', modify);
+function modify(event){
+    event.preventDefault();
+alert(entry_id)
+    new_name = document.getElementById('new_name').value;
+    new_description = document.getElementById('new_desc').value;
+
+    fetch('https://diary-2018.herokuapp.com/api/v1/entries/'+parseInt(entry_id),{
+        method:'PUT',
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token')
+        },
+        body:JSON.stringify({
+            name:new_name,
+            desc:new_description
+        })
+    }).then(response=>{
+            status_code = response.status
+            return response.json()
+        }).then(response=>{
+            if(status_code !=200){
+                alert(response['Error'])
+            }
+            if(status_code == 401){
+                alert('Your session has expired. please login again');
+                window.location='../index.html'
+            }
+            if(status_code == 200){
+                alert('Entry successfully modified')
+                window.location='dashboard.html'
+            }
+        })
+
+}
 
 }
 
 function modify_entry(entry_id) {
-    alert(entry_id)
+
+
 }
